8422dd23e5adaac276ab93335f9e6ec6b399749f
Edward A Maxedon <edadma@gmail.com>
2025-05-03 10:48:54 -0400

    unit tests

--- src/main/scala/io/github/edadma/datefns/Main.scala
+++
@@ -1,7 +0,0 @@
-package io.github.edadma.datefns
-
-@main def run(): Unit =
-  val testDate: Date = newDate(2023, 11, 25, 14, 30, 45, 678)
-  val result         = format(testDate, "EEEE, MMMM d, yyyy 'at' h:mm a")
-
-  println(result)
